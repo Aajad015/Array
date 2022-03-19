e1d6d8c2acab8146b53d0e2d657660057f97bd3d
Aajad015 <52133710+Aajad015@users.noreply.github.com>
2022-03-19 18:12:23 +0530

    Add files via upload

---
+++ SUMOFARRAY.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],i,n,sum=0;
+clrscr();
+printf("Enter the Element :");
+scanf("%d\n",&n);
+for(i=0;i<n;i++)
+{
+ scanf("%d",&a[i]);
+ // printf("output Element\n");
+}
+
+printf("Output Element\n");
+ for(i=0;i<n;i++)
+ {
+ printf("%d\n",a[i]);
+  sum=sum+a[i];
+
+ }
+printf("%d sum",sum);
+ getch();
+ }
